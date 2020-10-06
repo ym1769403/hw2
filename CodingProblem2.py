525d416ce90005fe80bad9435ae608fcea125935
ym1769403 <70408430+ym1769403@users.noreply.github.com>
2020-10-06 15:10:32 -0500

    Initial commit

---
+++ CodingProblem2.py
@@ -0,0 +1,24 @@
+# Yasir Mushtaque Id: 1769403
+month_list = {
+    'January': '1',
+    'February': '2',
+    'March': '3',
+    'April': '4',
+    'May': '5',
+    'June': '6',
+    'July': '7',
+    'August': '8',
+    'September': '9',
+    'October': '10',
+    'November': '11',
+    'December': '12'
+    }
+
+
+input_date = input('Month day, year')
+if input_date != '-1':
+    date_split = input_date.split(',')
+    month_day, year = date_split[0], date_split[1]
+    month_split = month_day.split(' ')
+    month, day = month_list[month_split[0]], month_split[1]
+    print('{}/{}/{}'.format(month, day, year))
