536b55cb8a2ee15b7e4070de50b1c535dfcd9254
ym1769403 <70408430+ym1769403@users.noreply.github.com>
2020-10-06 15:37:03 -0500

    part b

--- CodingProblem2.py
+++ CodingProblem2.py
@@ -14,11 +14,14 @@ month_list = {
     'December': '12'
     }
 
+with open('inputDates.txt', 'r') as f:
+    for line in f:
+        if line == '-1':
+            break
 
-input_date = input('Month day, year')
-if input_date != '-1':
-    date_split = input_date.split(',')
-    month_day, year = date_split[0], date_split[1]
-    month_split = month_day.split(' ')
-    month, day = month_list[month_split[0]], month_split[1]
-    print('{}/{}/{}'.format(month, day, year))
+            date_split = input_date.split(',')
+            month_day, year = date_split[0], date_split[1]
+            month_split = month_day.split(' ')
+            month, day = month_list[month_split[0]], month_split[1]
+            print('{}/{}/{}'.format(month, day, year))
+
